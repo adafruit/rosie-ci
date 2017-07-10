56a68dc9dac4030aa1c4d8dd946d07699a0c8be7
Scott Shawcroft <scott.shawcroft@gmail.com>
2017-07-10 12:06:59 -0700

    Update requirements.txt with redis deps.

--- start.sh
+++ start.sh
@@ -1,7 +1,6 @@
 # source env.sh # Not in repo because it contains secrets.
 # redis-server
 # celery -A mike-ci.celery
-# celery -A mike-ci.celery -Q start
 export FLASK_APP=mike-ci.py
 flask run
 # ngrok http -subdomain=mike-ci 5000
